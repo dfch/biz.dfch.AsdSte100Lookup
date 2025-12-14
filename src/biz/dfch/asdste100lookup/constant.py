15c7e8c9caa68523ef28d83ab011ff530e3b0203
Ronald Rink <ronald.rink@d-fens.net>
2025-12-14 16:07:37 +0100

    version increase to v1.7.1

--- src/biz/dfch/asdste100lookup/constant.py
+++ src/biz/dfch/asdste100lookup/constant.py
@@ -23,7 +23,7 @@ class Constant:
     """System-wide constants."""
 
     # Note: also adjust in pyproject.toml.
-    _VERSION = "1.7.0"
+    _VERSION = "1.7.1"
     PROG_NAME = "AsdSte100Lookup"
 
     BLOCKING_WHITE_SPACE = "\u200b"
